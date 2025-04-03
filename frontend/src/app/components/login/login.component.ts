5a87ee76635130ebd7013b99eab5ba1253253da8
diegoseg15 <diegofersv@gmail.com>
2025-04-04 01:55:28 +0200

    :sparkles: Add example login functionality

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // 👈 Importás FormsModule acá
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true, 
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
 
+  onSubmit(): void {
+    if (this.email === 'admin' && this.password === '1234') {
+      this.successMessage = 'Login successful!';
+      this.errorMessage = '';
+
+    } else {
+      this.errorMessage = 'Invalid username or password';
+      this.successMessage = '';
+    }
+  }
 }
