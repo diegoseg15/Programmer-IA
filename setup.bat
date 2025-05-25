bed6e74c89a65207e5a0779814c6d99302e925b7
diegoseg15 <diegofersv@gmail.com>
2025-05-25 20:28:44 +0200

    :hammer: Improve setup.bat for better app startup experience

--- setup.bat
+++ setup.bat
@@ -2,36 +2,38 @@
 set ACTION=%1
 
 if "%ACTION%"=="install" (
-    echo 🔧 Instalando dependencias de Angular y Django...
+    echo 🔧Instalando dependencias de Angular y Django...
 
-    echo 📦 Instalando dependencias de Angular...
+    echo 📦Instalando dependencias de Angular...
     cd frontend
-    npm install
+    call npm install
     cd ..
 
-    echo 📦 Instalando dependencias de Django...
-    cd backend/backend
-    pip install -r requirements.txt
-    cd ..
+    echo 📦Instalando dependencias de Django...
+    cd backend\backend
+    call pip install -r requirements.txt
+    cd ..\..
 
-    echo ✅ Instalación completa.
+    echo ✔Instalación completa.
     goto end
 )
 
 if "%ACTION%"=="start" (
-    echo 🚀 Levantando los servicios...
+    echo 🚀Levantando los servicios...
     docker-compose up --build -d
-    echo ✅ Plataforma en ejecución. Visita http://localhost:4200
-)
-
-if "%ACTION%"=="stop" (
-    echo 🛑 Deteniendo los servicios...
-    docker-compose down
-    echo ✅ Servicios detenidos.
+    echo ✔Plataforma en ejecución. Visita http://localhost:4200
     goto end
 )
 
-echo Uso: setup.bat {install|start|stop}
+if "%ACTION%"=="stop" (
+    echo 🛑Deteniendo los servicios...
+    docker-compose down
+    echo ✔Servicios detenidos.
+    goto end
+)
+
+echo Porfavor, proporciona un argumento valido.
+echo Uso: "setup.bat {install|start|stop}"
 
 :end
 pause
