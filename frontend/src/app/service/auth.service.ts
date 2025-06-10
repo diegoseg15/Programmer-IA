292a17733c90c398a19a81513a9e1594257dc300
diegoseg15 <diegofersv@gmail.com>
2025-06-10 23:58:52 +0200

    :sparkles: Añadido servicio de autenticación con login, verificación y logout en Angular

---
+++ frontend/src/app/service/auth.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserLogin } from '../compartido/userlogin';
+import { Observable } from 'rxjs';
+import { baseURL } from '../compartido/baseurl';
+import { tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(userLogin: UserLogin): Observable<any> {
+    return this.http.post<any>(`${baseURL}/login/`, userLogin, httpOptions)
+      .pipe(
+        tap(response => {
+          if (response.token) {
+            localStorage.setItem('token', response.token);
+          }
+        })
+      );
+  }
+
+  // Verificar si el usuario está logueado
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  // Cerrar sesión
+  logout(): void {
+    localStorage.removeItem('token');
+  }
+
+  // Obtener el token
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+}
