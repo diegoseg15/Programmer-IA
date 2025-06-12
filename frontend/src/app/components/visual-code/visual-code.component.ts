d656d85601d168ab883c43df7b21262c6aa84d30
diegoseg15 <diegofersv@gmail.com>
2025-06-13 01:59:07 +0200

    :zap: Mejora en VisualCodeComponent para actualizar código y análisis al cambiar la selección

--- frontend/src/app/components/visual-code/visual-code.component.ts
+++ frontend/src/app/components/visual-code/visual-code.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, AfterViewChecked, ElementRef } from '@angular/core';
+import { Component, Input, AfterViewChecked, ElementRef, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 
 //Imports de prism
 import * as Prism from 'prismjs';
@@ -10,33 +12,73 @@ import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-scss';
 
-// declare var Prism: any;
-
 @Component({
   selector: 'app-visual-code',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './visual-code.component.html',
   styleUrls: ['./visual-code.component.css']
 })
-export class VisualCodeComponent implements AfterViewChecked {
-  @Input() codeData: string = '';
-  @Input() language: string = 'javascript';
-  @Input() analysis: string = '';
+export class VisualCodeComponent implements AfterViewChecked, OnInit {
+  @Input() isLoading: boolean = false;
+  @Input() messages: any[] = [];
 
-  constructor(private elRef: ElementRef) {}
+  selectedIndex: number = 0;
+  codeData: string = '';
+  language: string = 'Code';
+  analysis: string = '';
+
+  constructor(
+    private elRef: ElementRef,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    if (this.messages.length > 0) {
+      this.setMessageData(this.selectedIndex);
+    }
+  }
+
+  ngOnChanges() {
+    if (this.messages && this.messages.length > 0) {
+      this.selectedIndex = this.messages.length - 1
+      this.setMessageData(this.selectedIndex);
+    }
+  }
 
   ngAfterViewChecked(): void {
     this.highlightCode();
   }
 
-  private highlightCode(): void {
-    const codeElement = this.elRef.nativeElement.querySelector('.code-body pre code');
-    if (codeElement) {
-      codeElement.className = `language-${this.language.toLowerCase()}`;
-      Prism.highlightElement(codeElement);
-    }
+  onSelectChange(): void {
+    this.setMessageData(this.selectedIndex);
+    console.log("codeData: ", this.codeData);
+
   }
+
+  private setMessageData(index: number): void {
+    const selected = this.messages[index];
+    if (!selected) return;
+    this.codeData = (selected.code || '').replace(/\\n/g, '\n');
+
+    this.codeData = selected.code || '';
+    this.language = selected.lenguaje || 'text';
+    this.analysis = selected.mensaje || '';
+
+    this.cdr.detectChanges();
+    this.highlightCode();
+  }
+
+  highlightCode(): void {
+    setTimeout(() => {
+      const codeElement = this.elRef.nativeElement.querySelector('.code-body pre code');
+      if (codeElement) {
+        codeElement.className = `language-${this.language.toLowerCase()}`;
+        Prism.highlightElement(codeElement);
+      }
+    }, 0);
+  }
+
 
   get languageClass(): string {
     return `language-${this.language.toLowerCase()}`;
