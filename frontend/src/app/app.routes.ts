7be3d95ea3afef669957deee18fce039d4740c68
diegoseg15 <diegofersv@gmail.com>
2025-06-13 02:24:12 +0200

    :sparkles: Añadida ruta para historial de chats en el router ('chats/history')

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ChatHistoryModalComponent } from './components/chat-history-modal/chat-history-modal.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'chat/:id', component: HomeComponent },
+    {path: 'chats/history', component: ChatHistoryModalComponent}
 ];
