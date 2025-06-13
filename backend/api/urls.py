79a848f5237a2a38b02b6e25dc8b58a48ca16aef
diegoseg15 <diegofersv@gmail.com>
2025-06-13 02:31:08 +0200

    :sparkles: Añadidas rutas para actualizar y eliminar mensajes por ID

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .Views.chat_view import ChatView, SaveMessageView, GetMessagesView, GetMessageByIdView
-from .Views.auth_views import LoginView, RegisterView
+from .Views.chat_view import ChatView, SaveMessageView, GetMessagesView, GetMessageByIdView, UpdateMessageView, DeleteMessageView
+from .Views.auth_views import LoginView, RegisterView, GetUserActiveView
 
 urlpatterns = [
     path('chat/', ChatView.as_view(), name='chat'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
     path('save-message/', SaveMessageView.as_view(), name='save_message'),
     path('messages/', GetMessagesView.as_view(), name='get_messages'),
     path('message/<str:id>/', GetMessageByIdView.as_view(), name='get_message_by_id'),
+    path('message/update/<str:id>/', UpdateMessageView.as_view(), name='update_message'),
+    path('delete-message/<str:id>/', DeleteMessageView.as_view(), name='delete-message'),
+
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('user_auth/', GetUserActiveView.as_view(), name='get_user_auth')
 ]
