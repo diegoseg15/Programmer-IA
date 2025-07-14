bca61113f6c8df0170ddb651cdd1770702679177
diegoseg15 <diegofersv@gmail.com>
2025-07-14 18:22:00 +0200

    :sparkles: Se añadió la ruta para el registro de usuarios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ChatHistoryModalComponent } from './components/chat-history-modal/chat-history-modal.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'chat/:id', component: HomeComponent },
     {path: 'chats/history', component: ChatHistoryModalComponent}
 ];
